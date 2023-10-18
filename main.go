6bd692de0f2dd9f4e35f25594e7bf64045ea87cd
joshua468 <112911898+joshua468@users.noreply.github.com>
2023-10-18 16:34:58 +0100

    Update main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -22,7 +23,7 @@ func main() {
 }
 func checkDomain(domain string) {
 	var hasMX, hasSPF, hasDMARC bool
-	var sprRecord, dmarcRecord string
+	var spfRecord, dmarcRecord string
 
 	mxRecords, err := net.LookupMX(domain)
 	if err != nil {
@@ -31,10 +32,29 @@ func checkDomain(domain string) {
 	if len(mxRecords) > 0 {
 		hasMX = true
 	}
-	t
-	t.Records := net.LookupTXT(domain)
-	if err!= nil{
-		log.Printf("Error:%v\n",err)
-		
+
+	txtRecords, err := net.LookupTXT(domain)
+	if err != nil {
+		log.Printf("Error:%v\n", err)
 	}
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+	}
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)                    
+	}
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			dmarcRecord = record
+		}
+	}
+	fmt.Printf("%v,%v,%v,%v,%v,%v", domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
 }
+
